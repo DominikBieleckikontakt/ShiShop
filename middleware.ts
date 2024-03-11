2d0125d60f0979440dd4957a2d7a4ac516ec768e
DominikBieleckikontakt <dominikbielecki.kontakt@gmail.com>
2024-03-11 09:32:57 +0100

    tried to add genius auth

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/shop", "/api/webhooks/route"],
-  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)"],
+  publicRoutes: ["/", "/shop", "/api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
